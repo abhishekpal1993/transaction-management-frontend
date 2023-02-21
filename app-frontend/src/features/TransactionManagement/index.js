ab5b3e208ba7556d669cda833554cfdb9e86ab64
abhishek.pal <48048153+abhishekpal1993@users.noreply.github.com>
2023-02-22 01:43:26 +0530

    responsive UI designs

--- app-frontend/src/features/TransactionManagement/index.js
+++ app-frontend/src/features/TransactionManagement/index.js
@@ -16,12 +16,12 @@ export const TransactionManagement = ({
     }
   );
   return (
-    <div className="grid grid-cols-3 min-h-screen">
+    <div className="grid grid-cols-1 md:grid-cols-3 min-h-screen">
       <div className="flex flex-col items-start border py-2 px-4 w-full">
         <SectionHeader className="mb-2">Submit new transaction</SectionHeader>
         <TransactionForm onSubmit={onSubmitHandler} isLoading={isLoading} />
       </div>
-      <div className="col-span-2 flex flex-col items-start border py-2 px-4 w-full">
+      <div className="col-span-1 md:col-span-2 flex flex-col items-start border py-2 px-4 w-full">
         <SectionHeader className="mb-2">Transaction history</SectionHeader>
         <TransactionList data={transactionList} />
       </div>
