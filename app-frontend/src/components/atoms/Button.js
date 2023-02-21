7c4ad6d32c65099e6ef6f80b9a7494773bb4e307
abhishek.pal <48048153+abhishekpal1993@users.noreply.github.com>
2023-02-21 21:47:49 +0530

    allow for clicks on submit when loading

--- app-frontend/src/components/atoms/Button.js
+++ app-frontend/src/components/atoms/Button.js
@@ -9,10 +9,9 @@ export const Button = ({
   ...props
 }) => {
   const defaultClassName = "shadow-md text-white font-bold py-2 px-4 rounded";
-  const isDisabledClassName =
-    !!isLoading || !!disabled
-      ? "bg-gray-400 hover:bg-gray-700 cursor-not-allowed"
-      : "bg-blue-500 hover:bg-blue-700 cursor-pointer";
+  const isDisabledClassName = !!disabled
+    ? "bg-gray-400 hover:bg-gray-700 cursor-not-allowed"
+    : "bg-blue-500 hover:bg-blue-700 cursor-pointer";
   return (
     <input
       {...props}
